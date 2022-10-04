d27d176fbfc9cfc5e8bf160ecf9c7b95b5d689f4
Dhruv Mohindru <dhruv.mohindru@parcelpoint.com.au>
2022-10-04 11:45:39 +1100

    created config server for this experiment

---
+++ config_server_example/test-config-server/src/main/java/dev/dmohindru/configserver/TestConfigServerApplication.java
@@ -0,0 +1,15 @@
+package dev.dmohindru.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class TestConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestConfigServerApplication.class, args);
+	}
+
+}
---
+++ config_server_example/test-config-server/src/test/java/dev/dmohindru/configserver/TestConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmohindru.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
